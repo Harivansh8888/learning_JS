ce2fdcf94f848df6f2ae8ba8b21c836fa86b186f
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-02-16 20:51:37 +0530

    added returning name of host of current URL

--- locationobj.js
+++ locationobj.js
@@ -7,10 +7,13 @@
 <h3>The window.location object</h3>
 
 <p id="demo"></p>
+<p id="demo1"></p>
 
 <script>
 document.getElementById("demo").innerHTML = 
 "The full URL of this page is:<br>" + window.location.href;
+document.getElementById("demo1").innerHTML = 
+"Page hostname is: " + window.location.hostname;
 </script>
 
 </body>
