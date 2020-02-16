9a53a2a5702b22a176b0414a7e56fe17f3a2e322
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-02-16 20:41:01 +0530

    It prints the width and height of the screen
    here screen refers to the computer in which the browser is opened. (BOM)

---
+++ screen_dimensions.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+<p id="demo1"></p>
+<p id="demo2"></p>
+
+<script>
+document.getElementById("demo1").innerHTML = 
+"Screen width is " + screen.width;
+document.getElementById("demo2").innerHTML = 
+"Screen height is " + screen.height;
+</script>
+
+</body>
+</html>
