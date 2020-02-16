f9d51d319999e5d4b247e00aff92d4876f92f406
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-02-16 20:54:10 +0530

    added displaying web protocol of current URL

--- locationobj.js
+++ locationobj.js
@@ -8,12 +8,15 @@
 
 <p id="demo"></p>
 <p id="demo1"></p>
+<p id="demo2"></p>
 
 <script>
 document.getElementById("demo").innerHTML = 
 "The full URL of this page is:<br>" + window.location.href;
 document.getElementById("demo1").innerHTML = 
 "Page hostname is: " + window.location.hostname;
+document.getElementById("demo2").innerHTML = 
+"Page protocol is: " + window.location.protocol;
 </script>
 
 </body>
