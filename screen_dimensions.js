074ba64fb032d26e1a2cc14efbd372e277ce100c
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-02-16 20:44:28 +0530

    added available width and height module

--- screen_dimensions.js
+++ screen_dimensions.js
@@ -9,6 +9,11 @@ document.getElementById("demo1").innerHTML =
 "Screen width is " + screen.width;
 document.getElementById("demo2").innerHTML = 
 "Screen height is " + screen.height;
+
+document.getElementById("demo1").innerHTML =
+"Available screen width is " + screen.avail.Width;
+document.getElementById("demo2").innerHTML = 
+"Available screen height is " + screen.availHeight;
 </script>
 
 </body>
