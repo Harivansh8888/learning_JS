0c79a92225f1bd724d0e9ab93fbf6e775bb9246a
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-02-17 18:47:45 +0530

    add module that loads next URL in history

--- window_history.js
+++ window_history.js
@@ -4,11 +4,15 @@
 function goBack() {
   window.history.back()
 }
+function goForward() {
+  window.history.forward()
+}
 </script>
 </head>
 <body>
 
 <input type="button" value="Back" onclick="goBack()">
+<input type="button" value="Forward" onclick="goForward()">
 
 </body>
 </html>
