dea036b19d9237ac4ef1522e49c31ae0b1b86e46
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-02-17 19:00:10 +0530

    added module returning app name of browser

--- window_navigator.js
+++ window_navigator.js
@@ -8,9 +8,14 @@
 
 <p id="demo"></p>
 
+<p>The appName property returns the application name of the browser:</p>
+
+<p id="demo1"></p>
 <script>
 document.getElementById("demo").innerHTML =
 "navigator.cookieEnabled is " + navigator.cookieEnabled;
+document.getElementById("demo1").innerHTML = 
+"navigator.appName is " + navigator.appName;
 </script>
 
 </body>
